c875e6d4027037590f3a549ff792528c5d1c6b48
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-28 18:14:23 +0600

    Send token server to client and client to the server side

--- __jwt_steps__.js
+++ __jwt_steps__.js
@@ -15,6 +15,13 @@
  * 1. set cookies with http only. for development secure: false,
  *
  * 2. cors
+ * app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
  *
  * 3. client side axios setting
+ * in axios set withCredentials: true
  */
--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bye0to9.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -40,7 +47,6 @@ async function run() {
         .cookie('token', token, {
           httpOnly: true,
           secure: false,
-          sameSite: 'none',
         })
         .send({ success: true });
     });
@@ -66,6 +72,8 @@ async function run() {
     // bookings
 
     app.get('/bookings', async (req, res) => {
+      console.log(req.query.email);
+      console.log('tok tok token', req.cookies.token);
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
